c45f7747c71a78979b06a2e90882ac6731bfe72d
samantamansurova <144723764+samantamansurova@users.noreply.github.com>
2024-05-02 21:02:53 +0300

    Update heap.py

--- heap.py
+++ heap.py
@@ -57,13 +57,16 @@ class MaxHeap:
 
 
 def find_kth_smallest(nums, k):
-    ###################################
-    ###                             ###
-    ###                             ###
-    ###                             ###
-    ###################################
+     
+    max_heap = MaxHeap()
 
-
+    for num in nums:
+        max_heap.insert(num)
+        
+        if len(max_heap.heap) > k:
+            max_heap.remove()
+            
+    return max_heap.heap[0]
 
 
 # Test cases
